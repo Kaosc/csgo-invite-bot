7e03a32b079a1c48b271315b3e8b4924f7311683
Kaosc <themelihbt@gmail.com>
2023-01-09 23:42:24 +0300

    update

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import Express from "express"
+
+const app = Express()
+
+app.all("/", (req, res) => {
+  res.send("+ Bot is running")
+})
+
+export function KeepAlive() {
+  app.listen(3000, () => console.log("+ Server is Ready"))
+}
