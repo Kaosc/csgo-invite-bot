58fbfbabf7ee554cc3286f514a5bbdc53a3d7f1e
Kaosc <themelihbt@gmail.com>
2023-01-16 00:50:45 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ dotenv.config()
 const BOT_USERNAME = process.env.TWITCH_BOT_USERNAME
 const ACCESS_TOKEN = process.env.TWITCH_ACCESS_TOKEN
 
-<<<<<<< HEAD
 const clientTMI = new tmi.Client({
 	options: {
 		debug: true,
@@ -38,34 +37,3 @@ client.on("connected", (addr, port) => {
 
 clientTMI.connect()
 KeepAlive()
-=======
-// Define configuration options
-const client = new tmi.Client({
-  options: {
-    debug: true,
-  },
-  connection: {
-    reconnect: true,
-    secure: true,
-  },
-  identity: {
-    username: BOT_USERNAME,
-    password: ACCESS_TOKEN,
-  },
-  channels: ["kaoscc"],
-})
-
-client.on("message", async (channel, tags, message, self) => {
-  // Ignore echoed messages.
-  if (self) return
-
-  if (message.toLowerCase() === "!code") {
-    // "@alca, heya!"
-    client.say(channel, `@${tags.username}, heya!`).catch(console.error)
-  }
-})
-
-client.connect()
-
-//KeepAlive()
->>>>>>> 08a11c1eaa18e8d6e3dd0a247460f1823de9921e
